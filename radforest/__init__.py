b1a8ab301496d2f43ad70ac752c3cd7351b310f8
des-biens <nicholas.desbiens@gmail.com>
2019-10-17 15:48:11 -0400

    init commit

---
+++ radforest/__init__.py
@@ -0,0 +1,15 @@
+"""
+Primitive module.
+
+This module is the core of the radforest library.
+"""
+
+class Primitive(object):
+    """Primitive class.
+    * to_radians
+    * from_radians
+    * to_json
+    * from_json
+    """
+    def __init__(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name = 'radforest',
+    version = '0.1.0',
+    description = 'A library to interact with forests...',
+    url = '',
+    author = 'Nicholas Desbiens',
+    author_email = 'nicholas@des-des-res.com',
+    license = 'MIT',
+    pakages = setuptools.find_packages(exclude=['test'])
+
+)
